0006e59e01ce6600e88e9974b03f218ec878e271
Vyacheslav Rybalchenko <rybco@outlook.com>
2025-02-14 20:57:45 +0300

    feat(DL-14): add alert-dialog for token deletion

--- src/widgets/api-token-list/ui/api-token-list-item/index.tsx
+++ src/widgets/api-token-list/ui/api-token-list-item/index.tsx
@@ -2,6 +2,17 @@ import { FC } from 'react';
 
 import { APITokenItem } from '@/entities/api-token-item/model';
 import { DeleteApiTokenButton } from '@/features/delete-api-token/ui';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/shared/shadcn/ui/alert-dialog';
 
 export const APITokenListItem: FC<APITokenItem> = ({ id, name, partialKey }) => {
   return (
@@ -14,7 +25,30 @@ export const APITokenListItem: FC<APITokenItem> = ({ id, name, partialKey }) => 
         </div>
       </td>
       <td className="p-2">
-        <DeleteApiTokenButton tokenId={id} />
+        <AlertDialog>
+          <AlertDialogTrigger>Delete</AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>
+                Are you sure that you want to delete this API Token?
+              </AlertDialogTitle>
+
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete the{' '}
+                <span className="whitespace-nowrap font-extrabold">{name}</span> API
+                Token.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+
+              <AlertDialogAction asChild>
+                <DeleteApiTokenButton tokenId={id} />
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </td>
     </tr>
   );
