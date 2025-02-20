75d64bdb8f01f4a82bd179b72ade3bd666f0e482
Vyacheslav Rybalchenko <rybco@outlook.com>
2025-02-20 20:43:01 +0300

    feat(DL-18): add dashboard page

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,3 +1,3 @@
-export default function Home() {
-  return <div></div>;
-}
+import { DashboardPage } from '@/views/dashboard/ui';
+
+export default DashboardPage;
---
+++ src/views/dashboard/ui/index.tsx
@@ -0,0 +1,9 @@
+import { ChartLOC } from '@/widgets/chart-loc/ui';
+
+export const DashboardPage = () => {
+  return (
+    <div>
+      <ChartLOC />
+    </div>
+  );
+};
