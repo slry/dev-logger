6a08956d27561dd7906c2d6f6aaa688c905ddafc
Vyacheslav Rybalchenko <rybco@outlook.com>
2025-03-21 16:32:42 +0300

    fix(chart): add gap in label

--- src/shared/shadcn/ui/chart.tsx
+++ src/shared/shadcn/ui/chart.tsx
@@ -219,7 +219,7 @@ const ChartTooltipContent = React.forwardRef<
                     )}
                     <div
                       className={cn(
-                        'flex flex-1 justify-between leading-none',
+                        'flex flex-1 justify-between gap-2 leading-none',
                         nestLabel ? 'items-end' : 'items-center',
                       )}
                     >
