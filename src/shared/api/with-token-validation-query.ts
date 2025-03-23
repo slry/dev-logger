9d0b56233604c478133c07d4fb7fde7eacd72dda
Vyacheslav Rybalchenko <rybco@outlook.com>
2025-03-23 17:41:52 +0300

    feat(with-token-validation-query): add hoc for token and body validation

---
+++ src/shared/api/with-token-validation-query.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { ZodType } from 'zod';
+
+import { validateToken } from './validate-token';
+import { parseBody } from '../lib/parseBody';
+
+type HandlerParams<T> = {
+  req: NextRequest;
+  token: string;
+  userId: string;
+  body: T;
+};
+
+type Handler<T> = (params: HandlerParams<T>) => Promise<NextResponse>;
+
+type WithTokenValidationQueryParams<T> = {
+  handler: Handler<T>;
+  bodySchema: ZodType<T>;
+};
+
+export const withTokenValidationQuery =
+  <T>({ handler, bodySchema }: WithTokenValidationQueryParams<T>) =>
+  async (req: NextRequest) => {
+    const { searchParams } = new URL(req.url);
+
+    const token = searchParams.get('token');
+
+    if (!token) {
+      return NextResponse.json('Missing token', { status: 400 });
+    }
+
+    const { data: userId, error } = await validateToken(token);
+
+    if (error || !userId) {
+      return NextResponse.json(error, { status: 400 });
+    }
+
+    const { data: body, error: bodyError } = await parseBody(req, bodySchema);
+
+    if (bodyError || !body) {
+      return NextResponse.json(bodyError, { status: 400 });
+    }
+
+    return handler({ req, token, userId, body });
+  };
