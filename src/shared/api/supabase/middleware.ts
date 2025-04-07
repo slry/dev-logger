5b1f50be06fc8e89c7a2f64a506497e14f9c9f4f
Vyacheslav Rybalchenko <rybco@outlook.com>
2025-04-07 17:35:11 +0300

    fix(middleware): respect basePath

--- src/shared/api/supabase/middleware.ts
+++ src/shared/api/supabase/middleware.ts
@@ -2,6 +2,8 @@ import { createServerClient } from '@supabase/ssr';
 import { NextResponse, type NextRequest } from 'next/server';
 
 export async function updateSession(request: NextRequest) {
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
   let supabaseResponse = NextResponse.next({
     request,
   });
@@ -37,19 +39,19 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (request.nextUrl.pathname.startsWith('/api')) {
+  if (request.nextUrl.pathname.startsWith(`${basePath}/api`)) {
     // no need to check for user on API routes
     return supabaseResponse;
   }
 
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/signup')
+    !request.nextUrl.pathname.startsWith(`${basePath}/login`) &&
+    !request.nextUrl.pathname.startsWith(`${basePath}/signup`)
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
-    url.pathname = '/login';
+    url.pathname = `${basePath}/login`;
     return NextResponse.redirect(url);
   }
 
