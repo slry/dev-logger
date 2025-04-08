c23273f6b880881c6afae5c8c9ba4b6a1e60b81f
Vyacheslav Rybalchenko <rybco@outlook.com>
2025-04-08 19:50:04 +0300

    feat(supabase): add realtime to tables

---
+++ supabase/migrations/2025040874700_add_realtime.sql
@@ -0,0 +1,4 @@
+alter publication supabase_realtime add table public.developer_file_operations;
+alter publication supabase_realtime add table public.developer_loc_per_day;
+alter publication supabase_realtime add table public.developer_loc_per_file;
+alter publication supabase_realtime add table public.developer_time_spent_per_day;
