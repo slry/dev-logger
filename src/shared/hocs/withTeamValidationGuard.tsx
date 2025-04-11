f18fbe6e01f0c4cae8d6d8c14b08143f3b1f72d6
Vyacheslav Rybalchenko <rybco@outlook.com>
2025-04-11 21:06:04 +0300

    fix(withTeamValidationGuard): pass validatedTeamId

--- src/shared/hocs/withTeamValidationGuard.tsx
+++ src/shared/hocs/withTeamValidationGuard.tsx
@@ -1,15 +1,22 @@
 import { redirect } from 'next/navigation';
+import { FC } from 'react';
 
 import { validateTeamId } from '../api/validate-team-id';
 
-export const withTeamValidationGuard = <T extends { teamId: string }>(
-  Component: React.FC<T>,
-) => {
-  const WithTeamValidationGuard = async (props: T) => {
-    const { teamId } = props as { teamId: string };
+interface ArgumentComponentProps {
+  teamId: string;
+}
+
+interface WithTeamValidationGuardProps {
+  params: Promise<{ teamId: string }>;
+}
+
+export const withTeamValidationGuard = (Component: FC<ArgumentComponentProps>) => {
+  const WithTeamValidationGuard: FC<WithTeamValidationGuardProps> = async (props) => {
+    const { teamId } = await props.params;
     const { valid, teamId: validatedTeamId } = await validateTeamId(teamId);
     if (!valid || !validatedTeamId) redirect('/team/personal/dashboard');
-    return <Component {...props} />;
+    return <Component teamId={validatedTeamId} />;
   };
 
   WithTeamValidationGuard.displayName = `WithTeamValidationGuard(${Component.displayName || Component.name})`;
